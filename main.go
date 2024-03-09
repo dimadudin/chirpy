78d89856cd3c06f2b092d6f6ae7f7a9731c4c917
dimadudin <dmitriy.i.dudin@gmail.com>
2024-03-09 16:59:59 +0300

    feat: hit count

--- main.go
+++ main.go
@@ -18,17 +18,43 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
-func readinessCheck(w http.ResponseWriter, r *http.Request) {
+func checkHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
 }
 
+type apiConfig struct {
+	fsHits int
+}
+
+func newApiConfig() apiConfig {
+	return apiConfig{fsHits: 0}
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fsHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getHits(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hits: ", cfg.fsHits)
+}
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) {
+	cfg.fsHits = 0
+}
+
 func main() {
 	mux := http.NewServeMux()
+	apiCfg := newApiConfig()
 	fsHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
-	mux.Handle("/app/*", fsHandler)
-	mux.HandleFunc("/healthz", readinessCheck)
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(fsHandler))
+	mux.HandleFunc("GET /reset", apiCfg.resetHits)
+	mux.HandleFunc("GET /metrics", apiCfg.getHits)
+	mux.HandleFunc("GET /healthz", checkHealth)
 	corsMux := middlewareCors(mux)
 	server := http.Server{Addr: "localhost:8080", Handler: corsMux}
 	err := server.ListenAndServe()
